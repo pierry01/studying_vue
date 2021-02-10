db91e0a94cd577247650b0f400b0413cfc5f135f
Jean Mendonça <pierrybm@gmail.com>
2021-02-10 13:10:32 -0300

    config vue toasted

---
+++ src/global.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+// const baseApiUrl = "http://localhost:3001"
+
+const showError = (e) => {
+  if (e && e.response && e.response.data)
+    Vue.toasted.global.defaultError({ msg: e.response.data });
+  else if (typeof e === "string") Vue.toasted.global.defaultError({ msg: e });
+  else Vue.toasted.global.defaultError();
+};
+
+export default { /* baseApiUrl, */ showError };
